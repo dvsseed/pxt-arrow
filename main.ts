f5b0349110ec12ef93ff5324cf85e798fc566dc4
曾令燊 <dvsseed@hotmail.com>
2017-12-28 20:54:35 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -11,43 +11,44 @@ namespace TaiBit {
     /**
       * Show arrows clockwise or counterclockwise
       * @param mode describe parameter here, eg: Clockwise, CounterClockwise
+      * @param speed describe parameter here, eg: 1000
     */
     //% weight=100 blockId="id_showArrow" block="show arrows direction | %mode"
-    export function fn_showArrow(mode: Directions): void {
+    export function fn_showArrow(mode: Directions, speed: number): void {
         if (mode == 1) {
             images.arrowImage(ArrowNames.North).showImage(0)
-            basic.pause(1000)
+            basic.pause(speed)
             images.arrowImage(ArrowNames.NorthEast).showImage(0)
-            basic.pause(1000)
+            basic.pause(speed)
             images.arrowImage(ArrowNames.East).showImage(0)
-            basic.pause(1000)
+            basic.pause(speed)
             images.arrowImage(ArrowNames.SouthEast).showImage(0)
-            basic.pause(1000)
+            basic.pause(speed)
             images.arrowImage(ArrowNames.South).showImage(0)
-            basic.pause(1000)
+            basic.pause(speed)
             images.arrowImage(ArrowNames.SouthWest).showImage(0)
-            basic.pause(1000)
+            basic.pause(speed)
             images.arrowImage(ArrowNames.West).showImage(0)
-            basic.pause(1000)
+            basic.pause(speed)
             images.arrowImage(ArrowNames.NorthWest).showImage(0)
-            basic.pause(1000)
+            basic.pause(speed)
         } else {
             images.arrowImage(ArrowNames.NorthWest).showImage(0)
-            basic.pause(1000)
+            basic.pause(speed)
             images.arrowImage(ArrowNames.West).showImage(0)
-            basic.pause(1000)
+            basic.pause(speed)
             images.arrowImage(ArrowNames.SouthWest).showImage(0)
-            basic.pause(1000)
+            basic.pause(speed)
             images.arrowImage(ArrowNames.South).showImage(0)
-            basic.pause(1000)
+            basic.pause(speed)
             images.arrowImage(ArrowNames.SouthEast).showImage(0)
-            basic.pause(1000)
+            basic.pause(speed)
             images.arrowImage(ArrowNames.East).showImage(0)
-            basic.pause(1000)
+            basic.pause(speed)
             images.arrowImage(ArrowNames.NorthEast).showImage(0)
-            basic.pause(1000)
+            basic.pause(speed)
             images.arrowImage(ArrowNames.North).showImage(0)
-            basic.pause(1000)
+            basic.pause(speed)
         }
     }
 }
